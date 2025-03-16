1fde98400e08d9d22e82fe1174eb2ebff03f2340
ndortiz <ndortiz@bancolombia.com.co>
2025-03-16 14:27:20 -0500

    Ajuste para las pruebas y que funcione en docker

--- ms-cuentas/src/main/java/com/banco/mscuentas/infrastructure/adapter/ClienteRepositoryAdapter.java
+++ ms-cuentas/src/main/java/com/banco/mscuentas/infrastructure/adapter/ClienteRepositoryAdapter.java
@@ -2,6 +2,7 @@ package com.banco.mscuentas.infrastructure.adapter;
 
 import com.banco.mscuentas.domain.ClienteDTO;
 import com.banco.mscuentas.domain.exception.ClienteNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.HttpClientErrorException;
@@ -9,15 +10,18 @@ import org.springframework.web.client.HttpClientErrorException;
 @Component
 public class ClienteRepositoryAdapter {
 	private final RestTemplate restTemplate;
-	private final String clienteServiceUrl = "http://localhost:8081/clientes/";
+	private final String clienteServiceUrl;
 
-	public ClienteRepositoryAdapter(RestTemplate restTemplate) {
+	public ClienteRepositoryAdapter(RestTemplate restTemplate, 
+	                                @Value("${cliente-service.url}") String clienteServiceUrl) {
 		this.restTemplate = restTemplate;
+		this.clienteServiceUrl = clienteServiceUrl;
+		System.out.println("🚀 URL del servicio clientes: " + clienteServiceUrl);
 	}
 
 	public boolean clienteExiste(Long clienteId) {
 		try {
-			restTemplate.getForObject(clienteServiceUrl + clienteId, Object.class);
+			restTemplate.getForObject(clienteServiceUrl + "/" + clienteId, Object.class);
 			return true;
 		} catch (HttpClientErrorException.NotFound e) {
 			return false;
@@ -26,7 +30,7 @@ public class ClienteRepositoryAdapter {
 
 	public ClienteDTO obtenerCliente(Long clienteId) {
 		try {
-			return restTemplate.getForObject(clienteServiceUrl + clienteId, ClienteDTO.class);
+			return restTemplate.getForObject(clienteServiceUrl + "/" + clienteId, ClienteDTO.class);
 		} catch (HttpClientErrorException.NotFound e) {
 			throw new ClienteNotFoundException(clienteId);
 		}
